be71eaa27042dbb3511eeae82cc42bf2012f2984
Nyaku <89752788+0xNyaku@users.noreply.github.com>
2022-07-24 03:43:01 +0100

    Update specterhub.lua

--- specterhub.lua
+++ specterhub.lua
@@ -1,3 +1,3 @@
 if game.PlaceId==3978370137 or game.PlaceId==1730877806 then 
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/gpofreeravenhub.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/gpospecterhub.lua"))()
 end
