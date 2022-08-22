d203e8ea9375381de37c700af09fff429818da4b
Nyaku <89752788+0xNyaku@users.noreply.github.com>
2022-08-22 21:43:17 +0100

    Update kayzhub.lua

--- kayzhub.lua
+++ kayzhub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId==3978370137 or game.PlaceId==1730877806 then 
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/gpospecterhub.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/gpokayzhub.lua"))()
 end
 if game.PlaceId==7465136166 or game.PlaceId==7465136166 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/secondsea.lua"))()
