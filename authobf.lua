85b93be5b61a7b05f0c91672dd63bacdaa3299ad
Nyaku <89752788+0xNyaku@users.noreply.github.com>
2022-08-22 20:53:45 +0100

    Update authobf.lua

--- authobf.lua
+++ authobf.lua
@@ -1,7 +1,35 @@
-local key = _G.Key
-local check = "https://www.fallen-angel.me/check.php?key=" .. key
-if game:HttpGet(check) == "Whitelisted!" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/0xNyaku/ravenhub/main/specterhub.lua"))()
-else
-    game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+_G.Key = "Key1"
+
+local keys = {
+    "p2xsw0enbj", --Nyaku
+    "k89lgg4oq5", --Kayz
+    "r9fbdbbdae"
+}
+
+local counter = 1
+local keyCheck
+for i,v in pairs(keys) do
+    if counter == #keys then
+    --not whitelisted!
+    keys = ""
+    game.Players.LocalPlayer:Kick("Not a valid key!")
+    else
+        if v == _G.Key then
+            --Whitelisted!
+            print("Successfully whitelisted!")
+            keyCheck = _G.Key
+            keys = ""
+        else
+            counter = counter +1
+        end
+    end
+end
+
+while true do
+    if _G.Key == keyCheck then
+        --Not spoofed
+    else
+        game.Players.LocalPlayer:Kick("Do not try and spoof your key!")
+    end
+    wait()
 end
